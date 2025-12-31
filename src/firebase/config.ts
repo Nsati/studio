07e1b0864730cb12faf88bf7d69a59e115ce73f7
Nitin Sati <nitin_sati@ql2.com>
2025-12-31 12:18:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1044710105-c2081",
+  "appId": "1:1039949913714:web:8903e0f740c2eb5989aceb",
+  "apiKey": "AIzaSyCb69encQoGQp0QR37p5cuhi0yNKwuuAB8",
+  "authDomain": "studio-1044710105-c2081.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1039949913714"
+};
