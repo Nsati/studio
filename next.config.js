aaa53f5dbab77c534f1afcf23d375d13755afa42
Nitin Sati <nitin_sati@ql2.com>
2026-01-20 10:43:20 +0000

    bhai Error: Invalid src prop (https://geof.storyboardthat.com/storyboard

--- next.config.js
+++ next.config.js
@@ -21,6 +21,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'geof.storyboardthat.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
