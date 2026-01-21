cd25cafaab5dba07cada78365af971b98235b189
Nitin Sati <nitin_sati@ql2.com>
2026-01-21 06:56:32 +0000

    7 PERMISSION_DENIED: Missing or insufficient permissions. nhi ho rha iss

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -7,7 +7,7 @@ export type SecurityRuleContext = {
 export class FirestorePermissionError extends Error {
   public context: SecurityRuleContext;
   constructor(context: SecurityRuleContext) {
-    const message = `FirestoreError: Missing or insufficient permissions: The following request was denied by Firestore Security Rules:\n${JSON.stringify(
+    const message = `FirestoreError: Missing or insufficient permissions. The following request was denied by Firestore Security Rules. This is a feature, not a bug. Please examine your rules and the request context below to solve it.\n\n${JSON.stringify(
       context,
       null,
       2
