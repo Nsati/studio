09d3ae96f517bec59e0fea294befb43e3d11c64a
Nitin Sati <nitin_sati@ql2.com>
2026-01-21 11:18:19 +0000

    Error: Invalid src prop (https://image2url.com/r2/default/images/1768992

--- next.config.js
+++ next.config.js
@@ -27,6 +27,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'image2url.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
