610323608e5306a09dcd42ef7d0e63536e940f44
Nitin Sati <nitin_sati@ql2.com>
2026-01-23 09:29:17 +0000

    [Meta pixel] 1007882680929031 is unavailable on this website due to it's

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,21 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        {/* Google Tag Manager - The primary container for all other tracking scripts */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M26M84QX');
-          `,
-          }}
-        />
-
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -48,16 +33,6 @@ export default function RootLayout({
         />
       </head>
       <body className={cn('font-body antialiased')}>
-        {/* GTM noscript fallback */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M26M84QX"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-
         <FirebaseClientProvider>
           <FirebaseErrorListener />
           <div className="flex min-h-screen flex-col">
