39aef32153c29c5dc943785eb6171d51836091b7
Nitin Sati <nitin_sati@ql2.com>
2026-01-27 12:16:13 +0000

    Su Mo Tu We Th Fr Sa   1  2  3 4   5    6   7   8   es format m hona cha

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -21,7 +21,7 @@ function Calendar({
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-2",
+        month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
@@ -31,7 +31,7 @@ function Calendar({
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
-        table: "w-full border-collapse space-y-2",
+        table: "w-full border-collapse",
         head_row: "flex",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
