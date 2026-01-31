2ff0e290069f053e52ca52ccfa6fd740e219ddc6
Nitin Sati <nitin_sati@ql2.com>
2026-01-31 07:30:10 +0000

    site ka ui theek kr de yr r

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ function FeaturedHotelsSkeleton() {
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
       {Array.from({ length: 4 }).map((_, i) => (
         <div key={i} className="space-y-4">
-          <Skeleton className="aspect-[4/5] w-full rounded-[2rem] md:rounded-[2.5rem]" />
+          <Skeleton className="aspect-[4/5] w-full rounded-[2.5rem] md:rounded-[2.5rem]" />
           <Skeleton className="h-6 w-3/4" />
           <Skeleton className="h-4 w-1/2" />
         </div>
